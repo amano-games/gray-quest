000202536b090418e2416c417a1b7c818792d9e6
Mario Carballo Zama <afk@ellugar.co>
2024-07-09 19:16:16 -0600

    feat: add crank support

--- source/main.lua
+++ source/main.lua
@@ -1,22 +1,36 @@
 import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/timer"
+import "CoreLibs/crank"
 
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
+local ticks_per_rev = 6
+local fps = 50
 local imgs = gfx.imagetable.new("imgs/test")
 assert(imgs)
 
+local function clamp(value, min, max)
+	return math.max(math.min(value, max), min)
+end
+
+
 local function init()
-	pd.display.setRefreshRate(50)
 end
 
 init()
 
 local t = 0
 function pd.update()
+	local ticks = pd.getCrankTicks(ticks_per_rev)
+
+	if ticks ~= 0 then
+		fps = clamp(fps + ticks, 20, 50)
+		pd.display.setRefreshRate(fps)
+	end
+
 	local frame = (t % 2) + 1
 	pd.timer.updateTimers()
 	imgs:drawImage(frame, 0, 0)
